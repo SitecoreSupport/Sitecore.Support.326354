0f121c96cfd1f03f82d04425ce61f8169df80cfa
Paul Stupka <paul.stupka@sitecore.com>
2019-04-24 13:26:45 +0300

    fix the issue

---
+++ src/Sitecore.Support.326354/ContentSearch/FieldReaders/LinkFieldReader.cs
@@ -0,0 +1,25 @@
+﻿namespace Sitecore.Support.ContentSearch.FieldReaders
+{
+  using Sitecore.Abstractions;
+  using Sitecore.ContentSearch;
+  using Sitecore.ContentSearch.FieldReaders;
+  using Sitecore.Data.Fields;
+  using Sitecore.Diagnostics;
+
+  [UsedImplicitly]
+  public class LinkFieldReader : FieldReader
+  {
+    public override object GetFieldValue(IIndexableDataField indexableField)
+    {
+      Field field = indexableField as SitecoreItemDataField;
+      BaseFieldTypeManager instance = ContentSearchManager.Locator.GetInstance<BaseFieldTypeManager>();
+      Assert.IsNotNull(instance, "fieldTypeManager != null");
+      if (instance.GetField(field) is LinkField)
+      {
+        return new LinkField(field).TargetID.ToGuid();
+      }
+
+      return null;
+    }
+  }
+}
