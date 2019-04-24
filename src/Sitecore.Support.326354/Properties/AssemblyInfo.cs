978c09c6cfd2770295ca26fcdf7349eed7089a1a
Paul Stupka <paul.stupka@sitecore.com>
2019-04-24 12:19:44 +0300

    init

---
+++ src/Sitecore.Support.326354/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.326354")]
+[assembly: AssemblyProduct("Sitecore.Support.326354")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("General Link field value is not indexed correctly")]
+
